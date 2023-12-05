c78b2ed58200487b5cdd797cab394ad2e1cf6a77
natasha <kutlyewa@yandex.ru>
2023-12-05 21:57:55 +0400

    svg & resources

--- gulpfile.js
+++ gulpfile.js
@@ -12,24 +12,10 @@ const newer = require('gulp-newer');//позволяет отслеживать 
 const browserSync = require('browser-sync').create();//синхронизируйте несколько браузеров и устройств
 const del = require('del');//удаление файлов
 
-//структура проекта
-const paths = {
-  html: {
-    src: 'src/*.html',
-    dest: 'dist/'
-  },
-  styles: {
-    src: 'src/styles/**/*.css',
-    dest: 'dist/css/'
-  },
-  scripts: {
-    src: 'src/scripts/**/*.js',
-    dest: 'dist/js/'
-  },
-  images: {
-    src: 'src/images/**',
-    dest: 'dist/img/'
-  }
+//перенос файлов
+function resources() {
+  return gulp.src('src/resources/**')
+    .pipe(gulp.dest('dist/'))
 }
 
 //очистка каталога dist
@@ -37,65 +23,66 @@ function clean() {
   return del(['dist/*', '!dist/img'])
 }
 
+//обработка html-файла
 function html() {
-  return gulp.src(paths.html.src)
+  return gulp.src('src/*.html')
     .pipe(htmlmin({
       collapseWhitespace: true
     }))
     .pipe(size({
       showFiles: true
     }))
-    .pipe(gulp.dest(paths.html.dest))
+    .pipe(gulp.dest('dist/'))
     .pipe(browserSync.stream());
 }
 
 //обработка стилей
 function styles() {
-  return gulp.src(paths.styles.src)
-  .pipe(sourcemaps.init())
-  .pipe(concat('style.min.css'))
-  .pipe(autoprefixer({
-    cascade: false
-  }))
-  .pipe(cleanCSS({
-    level: 2
-  }))
-  .pipe(sourcemaps.write('.'))
-  .pipe(size({
-    showFiles: true
-  }))
-  .pipe(gulp.dest(paths.styles.dest))
-  .pipe(browserSync.stream())
+  return gulp.src('src/css/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(cleanCSS({
+      level: 2
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(size({
+      showFiles: true
+    }))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream())
 }
 
 //обработка скриптов
 function scripts() {
-  return gulp.src(paths.scripts.src)
-  .pipe(sourcemaps.init())//карта источника
-  .pipe(babel({           //транспилирует код в старый формат
-    presets: ['@babel/env']
-  }))
-  .pipe(uglify())         //обфусцирование кода
-  .pipe(concat('script.min.js'))//переименоввывает
-  .pipe(sourcemaps.write('.'))   //карта источника
-  .pipe(size({                  //размер файлов в консоли
-    showFiles: true
-  }))
-  .pipe(gulp.dest(paths.scripts.dest))
-  .pipe(browserSync.stream())      //Синхронизирует
+  return gulp.src('src/js/**/*.js')
+    .pipe(sourcemaps.init())//карта источника
+    .pipe(babel({           //транспилирует код в старый формат
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())         //обфусцирование кода
+    .pipe(concat('script.min.js'))//переименоввывает
+    .pipe(sourcemaps.write('.'))   //карта источника
+    .pipe(size({                  //размер файлов в консоли
+      showFiles: true
+    }))
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(browserSync.stream())      //Синхронизирует
 }
 
 //обработка изображений
 function img() {
-  return gulp.src(paths.images.src)
-  .pipe(newer(paths.images.dest))
-  .pipe(imagemin({
-    progressive: true
-  }))
-  .pipe(size({
-    showFiles: true
-  }))
-  .pipe(gulp.dest(paths.images.dest))
+  return gulp.src('src/img/**')
+    .pipe(newer('dist/img/'))
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(size({
+      showFiles: true
+    }))
+    .pipe(gulp.dest('dist/img/'))
 }
 
 //отслеживать изменения
@@ -105,20 +92,21 @@ function watch() {
         baseDir: "./dist/"
     }
   })
-  gulp.watch(paths.html.dest).on('change', browserSync.reload)
-  gulp.watch(paths.html.src, html)
-  gulp.watch(paths.styles.src, styles)
-  gulp.watch(paths.scripts.src, scripts)
-  gulp.watch(paths.images.src, img)
+  gulp.watch('dist/').on('change', browserSync.reload)
+  gulp.watch('src/*.html', html)
+  gulp.watch('src/css/**/*.css', styles)
+  gulp.watch('src/js/**/*.js', scripts)
+  gulp.watch('src/img/**', img)
+  gulp.watch('src/resources/**', resources)
 }
 
-const build = gulp.series(clean, html, gulp.parallel(styles, scripts, img), watch)
+const build = gulp.series(clean, resources, html, gulp.parallel(styles, scripts, img), watch)
 
-exports.clean = clean
-exports.img = img
-exports.html = html
-exports.styles = styles
-exports.scripts = scripts
-exports.watch = watch
-exports.build = build
+// exports.clean = clean
+// exports.img = img
+// exports.html = html
+// exports.styles = styles
+// exports.scripts = scripts
+// exports.watch = watch
+// exports.build = build
 exports.default = build
